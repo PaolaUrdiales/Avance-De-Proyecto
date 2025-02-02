3162c7c3aea25c8e33fb371b8dcd32be34c29d0b
FridaGarzaG <162204353+FridaGarzaG@users.noreply.github.com>
2025-02-02 13:39:20 -0600

    Se crea un servidor en Express, agregando las librerias necesarias, la conexión a la base de datos y mensajes que muestren en consola que funciona correctamente.

---
+++ server.js
@@ -0,0 +1,36 @@
+//Variables de entorno
+require('dotenv').config();
+
+//Librerias
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const bcryptjs = require('bcryptjs');
+const path = require('path');
+const expressSession = require('express-session');
+const methodOverride = require('method-override'); // Para manejar el método PATCH
+
+const app = express(); //Iniciar app
+
+//Conexión a MySQL con variables de entorno
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+module.exports = connection;
+
+//Prueba de la conexión a la base de datos
+connection.connect((err) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos:', err);
+      return;
+    }
+    console.log('Conexión exitosa a la base de datos.');
+});
+
+app.listen(3000,()=>{
+    console.log('Server funciona en http://localHost:3000')
+})
